3e63d05a173499692b97447c0c2366ea1281016c
Yana Banana <Yana Banana@DESKTOP-7A4FNUF>
2021-03-15 14:14:26 +0200

    readme

--- script.py
+++ script.py
@@ -1,5 +1,22 @@
 import os
 import subprocess
-test = subprocess.Popen(["ping","-W","2","-c", "1", "192.168.1.70"], stdout=subprocess.PIPE)
-output = test.communicate()[0]
-os.system("ls -l")
+
+"""
+****os****
+"""
+
+# 
+os.system("ls") #output when everything is ok its return 0
+
+# os.popen will take the command as an input and return an open file object - you can do function on this object like read(), write().. 
+os.popen("ls").read() #output result of the command 'a.txt\ntest.py\n'
+
+print (os.popen("ls").read()) # will print it nicely not as a string 
+# spawn has multiplay name with different variation(type of arguments that can  be passed )
+os.spawnl(so.P_NOWAIT,"/usr/sh", "ls") #returns Process ID
+
+
+"""
+****subprocess modle****
+"""
+
